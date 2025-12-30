954da503174835b9db324207a95bde106f1804d6
Sean Bridges <sbridges@ruvos.com>
2025-12-30 08:02:32 -0700

    chore: use latest provider

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.48"
+      version = "~> 6.0"
     }
     template = {
       source  = "hashicorp/template"
